5441ccbab906159d28bda8d021823616dfc56757
Mona Lisa <125455854+swervolee@users.noreply.github.com>
2023-10-15 15:19:15 +0300

    a fix on unniteste

--- models/base_model.py
+++ models/base_model.py
@@ -39,7 +39,7 @@ class BaseModel():
                 self.updated_at = datetime.now()
         else:
             self.id = str(uuid.uuid4())
-            self.created_at = datetime.today()
+            self.created_at = datetime.now()
             self.updated_at = self.created_at
             models.storage.new(self)
 
